fd77a262c1454b13525f399bea9898212b820f97
EruBot <64939964+EruBot@users.noreply.github.com>
2021-06-09 12:07:59 +0700

    Update welcome.py

--- SaitamaRobot/modules/welcome.py
+++ SaitamaRobot/modules/welcome.py
@@ -1033,20 +1033,20 @@ NEW_MEM_HANDLER = MessageHandler(Filters.status_update.new_chat_members,
                                  new_member)
 LEFT_MEM_HANDLER = MessageHandler(Filters.status_update.left_chat_member,
                                   left_member)
-WELC_PREF_HANDLER = CommandHandler("welcome", welcome, filters=Filters.group.welcome)
-GOODBYE_PREF_HANDLER = CommandHandler("goodbye", goodbye, filters=Filters.group.goodbye)
-SET_WELCOME = CommandHandler("setwelcome", set_welcome, filters=Filters.group)
-SET_GOODBYE = CommandHandler("setgoodbye", set_goodbye, filters=Filters.group)
+WELC_PREF_HANDLER = CommandHandler("welcome", welcome, filters=Filters.group, async=True)
+GOODBYE_PREF_HANDLER = CommandHandler("goodbye", goodbye, filters=Filters.group, async=True)
+SET_WELCOME = CommandHandler("setwelcome", set_welcome, filters=Filters.group, async=True)
+SET_GOODBYE = CommandHandler("setgoodbye", set_goodbye, filters=Filters.group, async=True)
 RESET_WELCOME = CommandHandler(
-    "resetwelcome", reset_welcome, filters=Filters.group)
+    "resetwelcome", reset_welcome, filters=Filters.group, async=True)
 RESET_GOODBYE = CommandHandler(
-    "resetgoodbye", reset_goodbye, filters=Filters.group)
+    "resetgoodbye", reset_goodbye, filters=Filters.group, async=True)
 WELCOMEMUTE_HANDLER = CommandHandler(
-    "welcomemute", welcomemute, filters=Filters.group)
+    "welcomemute", welcomemute, filters=Filters.group, async=True)
 CLEAN_SERVICE_HANDLER = CommandHandler(
-    "cleanservice", cleanservice, filters=Filters.group)
+    "cleanservice", cleanservice, filters=Filters.group, async=True)
 CLEAN_WELCOME = CommandHandler(
-    "cleanwelcome", clean_welcome, filters=Filters.group)
+    "cleanwelcome", clean_welcome, filters=Filters.group, async=True)
 WELCOME_HELP = CommandHandler("welcomehelp", welcome_help)
 WELCOME_MUTE_HELP = CommandHandler("welcomemutehelp", welcome_mute_help)
 BUTTON_VERIFY_HANDLER = CallbackQueryHandler(user_button, pattern=r"user_join_")
